5abdfb8b521fff211212ce168e53588f1d3d9de5
mila <sunyuchen2014@gmail.com>
2025-09-12 01:09:41 +0800

    add chapter 1 with strange lombok problem

---
+++ chapter_1_init/webapp-demo/src/main/java/com/jerry/webappdemo/CommonResponse.java
@@ -0,0 +1,23 @@
+package com.jerry.webappdemo;
+
+public class CommonResponse {
+    private int status;
+    private String msg;
+
+    public CommonResponse() {
+        this(200, "");
+    }
+
+    public CommonResponse(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
